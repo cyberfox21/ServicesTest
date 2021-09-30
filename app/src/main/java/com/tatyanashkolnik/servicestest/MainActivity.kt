d21fa5b86a4e770cb7ad503ca11cd6c1fe6ae406
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2021-09-30 15:23:15 +0300

    Initial commit. Add coroutines dependencies and view binding.

---
+++ app/src/main/java/com/tatyanashkolnik/servicestest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tatyanashkolnik.servicestest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tatyanashkolnik.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
