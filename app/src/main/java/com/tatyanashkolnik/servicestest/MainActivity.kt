97714540e2766a17ad5b564bf69ce4244acdff3b
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2021-11-06 13:50:33 +0300

    Combine JobService with IntentService2.

--- app/src/main/java/com/tatyanashkolnik/servicestest/MainActivity.kt
+++ app/src/main/java/com/tatyanashkolnik/servicestest/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.tatyanashkolnik.servicestest
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
-import android.app.job.JobService
 import android.app.job.JobWorkItem
 import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.tatyanashkolnik.servicestest.MyJobService.Companion.JOB_ID
 import com.tatyanashkolnik.servicestest.databinding.ActivityMainBinding
@@ -53,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             val jobInfo = JobInfo.Builder(JOB_ID, componentName)
 //                .setExtras(MyJobService.newBundle(page++)) для вызова метода .schedule()
 //                .setRequiresCharging(true) // чтобы работал только на заряжающемся устройстве
-                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED) // только по WIFI
+//                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED) // только по WIFI
                 //ТОЛЬКО КОГДА БЕЗ ОЧЕРЕДИ
 //                .setPersisted(true) // выполнится даже после таго как телефон выключили и включили
                 .build()
@@ -61,11 +57,13 @@ class MainActivity : AppCompatActivity() {
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
 
 //            jobScheduler.schedule(jobInfo) // не складывает сервисы в очередь,
-                                           // просто отменяет предыдущий и запукает новый
+            // просто отменяет предыдущий и запукает новый
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val intent = MyJobService.newIntent(page++)
                 jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            } else {
+                startService(MyIntentService2.newIntent(this, page++))
             }
         }
     }
---
+++ app/src/main/java/com/tatyanashkolnik/servicestest/MyIntentService2.kt
@@ -0,0 +1,51 @@
+package com.tatyanashkolnik.servicestest
+
+import android.app.IntentService
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+
+class MyIntentService2 : IntentService(NAME) { // для комбинирования с JobScheduler
+    override fun onCreate() { // не делаем foreground потому что будем использовать его до версии 26
+        super.onCreate()  // а после 26 версии будем запускать уже JobScheduler
+        log("onCreate()")
+        setIntentRedelivery(true)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy()")
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+
+        val page = intent?.getIntExtra(PAGE, 0) ?: 0
+
+        log("onHandleIntent()")
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+        // после выполнения этого метода сервис будет остановлен автоматически
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyIntentService: $message")
+    }
+
+    companion object {
+
+        const val NAME = "intent_service"
+        const val PAGE = "page"
+
+        fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyIntentService2::class.java).apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
+}
